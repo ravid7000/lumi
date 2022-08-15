b46418590fd98c09a9fd810c86b74a14ebc961bf
Chahana Ravi <chahanaravi@Chahanas-MacBook-Pro.local>
2022-08-15 16:37:53 +0530

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+// app
+import App from 'app';
 
 ReactDOM.render(
   <React.StrictMode>
